890753b8a78a4ef38e8c5bceeb0089625f291276
Joel Campos <joelcamposoliva9000@gmail.com>
2022-08-16 18:32:12 +0200

    Fixed imports

---
+++ src/main/java/com/renua_sw/crm_v2/renua_sw_crm_v2/RenuaSwCrmV2Application.java
@@ -0,0 +1,18 @@
+package com.renua_sw.crm_v2.renua_sw_crm_v2;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenuaSwCrmV2Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenuaSwCrmV2Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/test/java/com/renua_sw/crm_v2/renua_sw_crm_v2/RenuaSwCrmV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renua_sw.crm_v2.renua_sw_crm_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenuaSwCrmV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
