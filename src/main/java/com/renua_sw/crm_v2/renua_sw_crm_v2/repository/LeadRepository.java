cb13e830157b1d0465531c37bb4ea472d29d33a4
Joel Campos <joelcamposoliva9000@gmail.com>
2022-08-16 21:21:30 +0200

    Base query (not working)

--- src/main/java/com/renua_sw/crm_v2/renua_sw_crm_v2/repository/LeadRepository.java
+++ src/main/java/com/renua_sw/crm_v2/renua_sw_crm_v2/repository/LeadRepository.java
@@ -1,9 +1,16 @@
 package com.renua_sw.crm_v2.renua_sw_crm_v2.repository;
 
 import com.renua_sw.crm_v2.renua_sw_crm_v2.model.Lead;
+import com.renua_sw.crm_v2.renua_sw_crm_v2.model.SalesRep;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LeadRepository extends JpaRepository<Lead, Integer> {
+
+    @Query("SELECT COUNT(id) as 'count', sales_rep FROM leads GROUP BY sales_rep")
+    List<Object[]> countAllBySalesRep();
 }
--- src/main/java/com/renua_sw/crm_v2/renua_sw_crm_v2/repository/OpportunityRepository.java
+++ src/main/java/com/renua_sw/crm_v2/renua_sw_crm_v2/repository/OpportunityRepository.java
@@ -1,9 +1,17 @@
 package com.renua_sw.crm_v2.renua_sw_crm_v2.repository;
 
+import com.renua_sw.crm_v2.renua_sw_crm_v2.enums.OpportunityStatus;
 import com.renua_sw.crm_v2.renua_sw_crm_v2.model.Opportunity;
+import com.renua_sw.crm_v2.renua_sw_crm_v2.model.SalesRep;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OpportunityRepository extends JpaRepository<Opportunity, Integer> {
+
+    @Query("SELECT COUNT(id) as 'count', sales_rep FROM opportunities GROUP BY sales_rep")
+    List<Object[]> countAllBySalesRep();
 }
