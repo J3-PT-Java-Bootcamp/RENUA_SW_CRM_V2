dc91c45f8f61f7553afa7aaf4ddb329ad42b0b3f
sjacas <sergi.jacas@wezaam.com>
2022-08-26 19:38:34 +0200

    Opportunity & Sales rep tests

--- src/test/java/com/ironhack/renua_sw_crm_v2/service/OpportunityServiceImplTest.java
+++ src/test/java/com/ironhack/renua_sw_crm_v2/service/OpportunityServiceImplTest.java
@@ -1,8 +1,14 @@
 package com.ironhack.renua_sw_crm_v2.service;
 
+import com.ironhack.renua_sw_crm_v2.Repository.LeadRepository;
+import com.ironhack.renua_sw_crm_v2.Repository.SalesRepRepository;
+import com.ironhack.renua_sw_crm_v2.enums.OpportunityStatus;
+import com.ironhack.renua_sw_crm_v2.error.NotFoundException;
+import com.ironhack.renua_sw_crm_v2.utils.TestDataService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,24 +19,34 @@ class OpportunityServiceImplTest {
     // Before run the test, please, remove or comment de line 45 from CRMStarterService
     // @EventListener(value = ApplicationReadyEvent.class)
 
+    @Autowired
+    TestDataService testDataService;
+
+    @Autowired
+    OpportunityService opportunityService;
+
+    @Autowired
+    SalesRepRepository salesRepRepository;
+
+    @Autowired
+    LeadRepository leadRepository;
+
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        testDataService.generateData();
     }
 
     @Test
-    void test_createFromLead_ok() {
+    void test_updateStatus_ok() throws NotFoundException {
+        opportunityService.updateStatus(1L, OpportunityStatus.CLOSED_WON);
+        var opportunityWon = opportunityService.getById(1L);
+        assertEquals(OpportunityStatus.CLOSED_WON, opportunityWon.getStatus());
     }
 
     @Test
-    void test_updateStatus_ok() {
-    }
-
-    @Test
-    void test_getById_ok() {
+    void test_getById_ok() throws NotFoundException {
+        var opportunity = opportunityService.getById(1L);
+        assertEquals(1L, opportunity.getId());
     }
 
     @Test
--- src/test/java/com/ironhack/renua_sw_crm_v2/service/SalesRepServiceImplTest.java
+++ src/test/java/com/ironhack/renua_sw_crm_v2/service/SalesRepServiceImplTest.java
@@ -1,8 +1,11 @@
 package com.ironhack.renua_sw_crm_v2.service;
 
+import com.ironhack.renua_sw_crm_v2.error.NotFoundException;
+import com.ironhack.renua_sw_crm_v2.utils.TestDataService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,20 +16,21 @@ class SalesRepServiceImplTest {
     // Before run the test, please, remove or comment de line 45 from CRMStarterService
     // @EventListener(value = ApplicationReadyEvent.class)
 
+    @Autowired
+    TestDataService testDataService;
+
+    @Autowired
+    SalesRepService salesRepService;
+
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        testDataService.generateData();
     }
 
     @Test
-    void test_createSalesRep_ok() {
-    }
-
-    @Test
-    void test_getById_ok() {
+    void test_getById_ok() throws NotFoundException {
+        var salesRep = salesRepService.getById(1L);
+        assertEquals(1L, salesRep.getId());
     }
 
     @Test
