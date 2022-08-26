6569bec2be4fc555a6e36b1c3ffe1c3c17c740eb
sjacas <sergi.jacas@wezaam.com>
2022-08-26 19:22:08 +0200

    Lead test

--- src/test/java/com/ironhack/renua_sw_crm_v2/service/LeadServiceImplTest.java
+++ src/test/java/com/ironhack/renua_sw_crm_v2/service/LeadServiceImplTest.java
@@ -1,8 +1,13 @@
 package com.ironhack.renua_sw_crm_v2.service;
 
-import org.junit.jupiter.api.AfterEach;
+import com.ironhack.renua_sw_crm_v2.Repository.LeadRepository;
+import com.ironhack.renua_sw_crm_v2.Repository.SalesRepRepository;
+import com.ironhack.renua_sw_crm_v2.error.NotFoundException;
+import com.ironhack.renua_sw_crm_v2.model.Lead;
+import com.ironhack.renua_sw_crm_v2.utils.TestDataService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,23 +18,35 @@ class LeadServiceImplTest {
     // Before run the test, please, remove or comment de line 45 from CRMStarterService
     // @EventListener(value = ApplicationReadyEvent.class)
 
+    @Autowired
+    TestDataService testDataService;
+
+    @Autowired
+    LeadService leadService;
+
+    @Autowired
+    SalesRepRepository salesRepRepository;
+
+    @Autowired
+    LeadRepository leadRepository;
+
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        testDataService.generateData();
     }
 
     @Test
-    void test_createLead_ok() {
+    void test_delete_ok() throws NotFoundException {
+        var firstSalesRep = salesRepRepository.findById(1L).orElseThrow();
+        var lead = new Lead("Robert", "682632654", "robert@mail.com", "Puma", firstSalesRep);
+        var leadSaved = leadRepository.save(lead);
+        leadService.delete(leadSaved);
+        assertThrows(NotFoundException.class, () -> {leadService.getById(leadSaved.getId());});
     }
 
     @Test
-    void test_delete_ok() {
-    }
-
-    @Test
-    void test_getById_ok() {
+    void test_getById_ok() throws NotFoundException {
+        var lead = leadService.getById(1L);
+        assertEquals(1L, lead.getId());
     }
 }
