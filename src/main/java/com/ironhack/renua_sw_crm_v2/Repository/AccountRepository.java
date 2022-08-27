a00397337e9bbe923ed68b4efbfc4237acf572d9
Joel Campos <joelcamposoliva9000@gmail.com>
2022-08-27 09:31:18 +0200

    Repo refactor

---
+++ src/main/java/com/ironhack/renua_sw_crm_v2/Repository/AccountRepository.java
@@ -0,0 +1,31 @@
+package com.ironhack.renua_sw_crm_v2.Repository;
+
+import com.ironhack.renua_sw_crm_v2.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    @Query("SELECT AVG(employeeCount) FROM Account")
+    Double meanEmployeeCount();
+
+    @Query("SELECT MAX(employeeCount) FROM Account")
+    Double maxEmployeecount();
+
+    @Query("SELECT MIN(employeeCount) FROM Account")
+    Double minEmployeecount();
+
+    @Query(value = "SELECT AVG(o.id) FROM opportunities o JOIN accounts a ON o.opportunity_account_id = a.id", nativeQuery = true)
+    Double meanOppsPerAccount();
+
+    @Query(value = "SELECT MAX(a.id) FROM opportunities o JOIN accounts a ON o.opportunity_account_id = a.id", nativeQuery = true)
+    Double maxOppsPerAccount();
+
+    @Query(value = "SELECT MIN(o.id) FROM opportunities o JOIN accounts a ON o.opportunity_account_id = a.id", nativeQuery = true)
+    Double minOppsPerAccount();
+
+}
