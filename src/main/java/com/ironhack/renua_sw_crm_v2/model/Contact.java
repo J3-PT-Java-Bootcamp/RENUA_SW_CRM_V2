89c06d9343a6bf2cbd606aa891e0ae83db138669
Joel Campos <joelcamposoliva9000@gmail.com>
2022-08-27 09:29:26 +0200

    Back roll

---
+++ src/main/java/com/ironhack/renua_sw_crm_v2/model/Contact.java
@@ -0,0 +1,29 @@
+package com.ironhack.renua_sw_crm_v2.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "contacts")
+public class Contact extends User {
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    @JsonIgnore
+    private Account account;
+
+    @OneToOne(mappedBy = "decisionMaker")
+    @JsonIgnore
+    private Opportunity opportunity;
+
+    public Contact(Lead lead) {
+        super(lead.name, lead.phoneNumber, lead.email, lead.companyName);
+    }
+}
