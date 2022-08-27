26ad1cbfa3fd99206dd199beae4a16291cb48766
Joel Campos <joelcamposoliva9000@gmail.com>
2022-08-27 09:28:27 +0200

    Interface

--- src/main/java/com/ironhack/renua_sw_crm_v2/service/AccountService.java
+++ src/main/java/com/ironhack/renua_sw_crm_v2/service/AccountService.java
@@ -10,6 +10,24 @@ import com.ironhack.renua_sw_crm_v2.model.SalesRep;
 import java.util.List;
 
 public interface AccountService {
+    Account createAccount(String companyName);
+    void show();
+    void show(Long id);
+    Account getById(Long id) throws NotFoundException;
+    void addOpportunityAndContact(Account account, Opportunity opportunity, Contact contact);
 
+    void meanEmployeecount();
+
+    void maxEmployeecount();
+
+    void minEmployeecount();
+
+    void meanOppsPerAccount();
+
+    void maxOppsPerAccount();
+
+    void minOppsPerAccount();
+
+    Account assignData(Contact decisionMaker, SalesRep salesRep, ProductType product, int trucksNum);
 
 }
